9847a34a015404fc0a2680b137172f81173efd6f
Kim Louise <kimlabrador71@gmail.com>
2024-11-10 17:09:53 +0800

    A Few Fixes

--- Scripts/00 Global/GameManager.cs
+++ Scripts/00 Global/GameManager.cs
@@ -3,12 +3,26 @@ using System;
 
 public partial class GameManager : Node
 {
-	public static GameManager gameManager{get; private set;}
+	public static GameManager gameManager {get; private set;}
+	
 	public override void _Ready()
 	{
 		gameManager = this;
 	}
 
 	public int health = 100;
-	
+		
+	public void TakeDamage(int damage)
+	{
+		if (health > 0)
+		{
+			health -= damage;
+		}
+
+		if (health <= 0)
+		{
+			health = 0;
+			GD.Print("You can't take damage!");
+		}
+	}
 }
