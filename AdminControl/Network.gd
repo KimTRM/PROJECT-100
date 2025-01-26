ce4e0a792a40a19bc805ab38c82f571ac3c8719f
Kim Louise <kimlabrador71@gmail.com>
2025-01-26 17:25:13 +0800

    Added a Flexible HTTPManager

--- AdminControl/Network.gd
+++ AdminControl/Network.gd
@@ -2,29 +2,25 @@ extends Node
 
 var http_request: HTTPRequest = HTTPRequest.new()
 
-const SERVER_URL = "http://localhost/project100/dbmediator.php"
-const SERVER_HEADERS = ["Content-Type: application/x-www-form-urlencoded", "Cache-Control: max-age=0"]
-
 var request_queue: Array = []
 var is_requesting: bool = false
 
-var tempt_data
+var tdata: Array = []
+
+const SERVER_URL = "http://localhost/project100/dbmediator.php"
+const SERVER_HEADERS = ["Content-Type: application/x-www-form-urlencoded", "Cache-Control: max-age=0"]
 
 func _ready():
 	add_child(http_request)
 	http_request.connect("request_completed", Callable(self, "_http_request_completed"))
 
 func _process(_delta):
-	check_request()
+	process_request_queue()
 
-func check_request():
-	# Check if we are good to send a request:
-	if is_requesting:
+func process_request_queue():
+	if is_requesting or request_queue.is_empty():
 		return
-		
-	if request_queue.is_empty():
-		return
-		
+	
 	is_requesting = true
 	_send_request(request_queue.pop_front())
 
@@ -33,15 +29,13 @@ func _send_request(request : Dictionary):
 	var data = client.query_string_from_dict({"data" : JSON.stringify(request['data'])})
 	var body = "command=" + request['command'] + "&" + data
 	
-	# Make request to the server:
 	var err = http_request.request(SERVER_URL, SERVER_HEADERS, HTTPClient.METHOD_POST, body)
 	
-	# Check if there were problems:
 	if err != OK:
 		printerr("HTTPRequest error: " + str(err))
+		is_requesting = false
 		return
 	
-	# Print out request for debugging:
 	print("Requesting...\n\tCommand: " + request['command'] + "\n\tBody: " + body)
 
 func _http_request_completed(_result, _response_code, _headers, _body):
@@ -50,18 +44,15 @@ func _http_request_completed(_result, _response_code, _headers, _body):
 		printerr("Error w/ connection: " + str(_result))
 		return
 		
-	var response_body = _body.get_string_from_utf8()
-	# Grab our JSON and handle any errors reported by our PHP code:
-	var test_json_conv = JSON.new()
-	test_json_conv.parse(response_body)
+	var response_body: String = _body.get_string_from_utf8()
+	var json: JSON = JSON.new()
+	var parse_result = json.parse(response_body)
 	
-	var response = test_json_conv.get_data()
-	
-	if response != null and response.has('error') and response['error'] != "none":
-		printerr("We returned error: " + response['error'])
+	if parse_result != OK:
 		return
-			
-	# If not requesting a nonce, we handle all other requests here:
+	
+	var response = json.parse_string(response_body)
+	tdata = response
 	print("Response Body:\n" + response_body)
 
 func submit_score():
