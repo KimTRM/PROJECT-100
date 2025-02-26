e48364330ab944c3cbdd60c3aa17878b82cc9a6d
Kim Louise <kimlabrador71@gmail.com>
2025-02-26 23:31:47 +0800

    Added a DragManager and DragBlock classes; implement drag-and-drop functionality for CodeBlocks (NOTE: NOT WORKING YET)

--- CodeBlocks/UI/BlockCanvas/BlockCanvas.cs
+++ CodeBlocks/UI/BlockCanvas/BlockCanvas.cs
@@ -1,15 +1,16 @@
+using System.Collections.Generic;
+using System.Linq;
 using Godot;
 using GodotUtilities;
-using System;
 
 [Scene]
 public partial class BlockCanvas : MarginContainer
 {
-    [Node ("WindowContainer/Overlay/MarginContainer/ZoomButtons/ZoomButton")]
+    [Node("WindowContainer/Overlay/MarginContainer/ZoomButtons/ZoomButton")]
     private Button ZoomButton;
 
-	[Node ("WindowContainer/Window")]
-	public Control Window;
+    [Node("WindowContainer/Window")]
+    public Control Window;
 
     private float zoomFactor = 1.0f;
     private float zoomStep = 0.1f;
@@ -19,6 +20,8 @@ public partial class BlockCanvas : MarginContainer
     private bool isDragging = false;
     private Vector2 dragStartPos;
     private Vector2 controlStartPos;
+
+    private List<CodeBlock> _codeBlocks = new List<CodeBlock>();
 
     public override void _Notification(int what)
     {
@@ -88,6 +91,8 @@ public partial class BlockCanvas : MarginContainer
         ZoomButton.Text = $"{zoomFactor:F1}x";
     }
 
+
+
     private async void _on_execute_pressed()
     {
         foreach (Control child in Window.GetChildren())
@@ -97,5 +102,48 @@ public partial class BlockCanvas : MarginContainer
                 await block.Execute();
             }
         }
+    }
+
+    public CodeBlock[] GetBlocks()
+    {
+        foreach (Control child in Window.GetChildren())
+        {
+            if (child is CodeBlock block)
+            {
+                _codeBlocks.Add(block);
+                return _codeBlocks.ToArray();
+            }
+        }
+
+        return null;
+    }
+
+    public void AddBlock(CodeBlock block)
+    {
+        Window.AddChild(block);
+        _codeBlocks.Add(block);
+    }
+
+    public void RemoveBlock(CodeBlock block)
+    {
+        _codeBlocks.Remove(block);
+        block.QueueFree();
+    }
+
+    public CodeBlock GetClosestBlock(Vector2 position, float snapThreshold = 50f)
+    {
+        return _codeBlocks
+            .Where(block => block != null && block.Visible)
+            .OrderBy(block => block.Position.DistanceTo(position))
+            .FirstOrDefault(block => block.Position.DistanceTo(position) < snapThreshold);
+    }
+
+    public void AttachBlock(CodeBlock block, CodeBlock targetBlock)
+    {
+        if (targetBlock == null) return;
+
+        block.Position = targetBlock.Position + new Vector2(0, targetBlock.Size.Y + 10); // Snap below target
+        _codeBlocks.Remove(block);
+        _codeBlocks.Insert(_codeBlocks.IndexOf(targetBlock) + 1, block);
     }
 }
