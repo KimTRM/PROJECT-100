593e54c2575bdbd1bde7a9a7b5bc5cec0917a71e
Kim Louise <kimlabrador71@gmail.com>
2025-03-02 13:10:54 +0800

    Removed the old GDScript Files

--- Scripts/UI/LoginMenu/login_panel.gd
+++
@@ -1,4 +0,0 @@
-extends PanelContainer
-
-@onready var username: LineEdit = $MarginContainer/VBoxContainer/MarginContainer2/VBoxContainer/PanelContainer/MarginContainer/VBoxContainer/Username
-@onready var password: LineEdit = $MarginContainer/VBoxContainer/MarginContainer2/VBoxContainer/PanelContainer2/MarginContainer/VBoxContainer/Password
